ec451fd6f4bb59525088ff552e5823b50777f341
Roy-sit <lpsroy78@gmail.com>
2024-11-19 00:04:11 -0500

    added images

--- js/script.js
+++ js/script.js
@@ -35,46 +35,6 @@ gsap.fromTo(
 
 
 
-// Zoom effect
-const promoImages = document.querySelectorAll("#mens-image, #showroom, #womens-image");
-
-promoImages.forEach(promoImage => {
-  const img = promoImage.querySelector("img");
-
-  promoImage.addEventListener("mouseenter", () => {
-    gsap.to(img, {
-      scale: 1.02, 
-      duration: 0.3,
-      ease: "power3.out",
-    });
-  });
-
-  promoImage.addEventListener("mousemove", (e) => {
-    const rect = promoImage.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / rect.width - 0.5) * 2; // Horizontal movement
-    const y = ((e.clientY - rect.top) / rect.height - 0.5) * 2; // Vertical movement
-
-    const maxMove = 10;
-    gsap.to(img, {
-      x: x * maxMove,
-      y: y * maxMove,
-      duration: 0.3,
-      ease: "power3.out",
-    });
-  });
-
-  promoImage.addEventListener("mouseleave", () => {
-    gsap.to(img, {
-      scale: 1,
-      x: 0,
-      y: 0,
-      duration: 0.3,
-      ease: "power3.out",
-    });
-  });
-});
-
-
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
@@ -93,8 +53,10 @@ gsap.utils.toArray(".full-width-grid-con").forEach((section) => {
     });
 });
 
-// Scale effect for images on scroll
-gsap.utils.toArray(" .vertical_text, #collection-image, #mens-image, #womens-image, #showroom, #earbuds-image").forEach((image) => {
+
+
+// Scroll effect
+gsap.utils.toArray("#m-model img, .vertical_text, #collection-image, #mens-image, #womens-image, #showroom, #earbuds-image").forEach((image) => {
     gsap.from(image, {
         scale: 0.8,
         opacity: 0,
