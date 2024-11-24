9c1c817bee54320360b750c9a20f73dcc4e5476c
Roy-sit <lpsroy78@gmail.com>
2024-11-24 01:35:39 -0500

    added more images to slider

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@
 const musicSection = document.querySelector(".music-section");
 const audio = document.querySelector(".music-section .background-music");
 
-audio.volume = 0.07;
+audio.volume = 0.08;
 
 function handleIntersection(entries) {
   entries.forEach((entry) => {
